e13379d4b4c48191b1664d392600f0adf48dee7f
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-07-15 14:55:03 -0700

    Using Templates

--- playground/views.py
+++ playground/views.py
@@ -7,5 +7,14 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-  return HttpResponse('Hello World')    # -> Map to a URL
+  #Ex.1
+  # return HttpResponse('Hello World')    # -> Map to a URL
+  
+  #ex.2
+  return render(request, 'hello.html', {'name': 'Mosh'} )
+  #(function) render: (request: HttpRequest, template_name: str | Sequence[str], context: Mapping[str, Any]
+  #Third parameter passes in a dictionary; use variable and value to insert into HTML page
+    #<h1>Hello {{ name }}</h1>
 
+  #ex.3
+  
